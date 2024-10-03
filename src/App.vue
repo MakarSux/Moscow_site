b58ffcad056564c90a323138f6d3d2d41e2b5388
MakarSux <mm.pyshkin@yandex.ru>
2024-10-03 12:07:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderComp from './components/HeaderComp.vue'
+
+</script>
+
+<template>
+  <header>
+    <HeaderComp />
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+</style>
